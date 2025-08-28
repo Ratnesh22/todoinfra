263636dd4e3e929452e80b6789e5064fd75745ba
Ratnesh Kumar (EXT-Nokia) <ratnesh.kumar.ext@nokia.com>
2025-08-28 23:21:17 +0530

    rg-added

---
+++ Environment/Dev/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "9c521f44-ae8d-4736-9337-a8ab0038c6c7"
+}
+
+resource "azurerm_resource_group" "rg_1" {
+  name     = "ratnesh-rg"
+  location = "West Europe"
+}
